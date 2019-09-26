0e5f81f4ee75ea470626285624106d07ef507c1c
thien061117 <ledinhthien1031@gmail.com>
2019-09-26 21:17:13 +0700

    design db

---
+++ db/migrate/20190926140446_create_tourist_areas.rb
@@ -0,0 +1,15 @@
+class CreateTouristAreas < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tourist_areas do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :introduce
+      t.boolean :status
+      t.references :user, foreign_key: true
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
